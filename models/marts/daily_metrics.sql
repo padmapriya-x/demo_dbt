b4f6ea1955bedb4ddfe331a9360a06ecf3d82931
Padma Priya <you@example.com>
2025-02-17 20:56:52 +0100

    :wq

--- models/marts/daily_metrics.sql
+++ models/marts/daily_metrics.sql
@@ -8,12 +8,12 @@ t2 as
 (
     select event_date,
     count(distinct USER_ID) as daily_active_users,
-    count(distinct (case when ITEM_ID is not null then user_id else 0 end)) as daily_active_learners,
-    count(distinct (case when EVENT_NAME = 'item-finished' then ITEM_ID else 0 end)) as content_completions,
-    count(distinct (case when EVENT_NAME = 'item-finished' then USER_ID else 0 end)) as content_completion_user_count,
+    count(distinct (case when ITEM_ID is not null then USER_ID else '0' end))-1 as daily_active_learners,
+    count(distinct (case when EVENT_NAME = 'item-finished' then ITEM_ID else '0' end))-1 as content_completions,
+    count(distinct (case when EVENT_NAME = 'item-finished' then USER_ID else '0' end))-1 as content_completion_user_count,
     count(distinct (case when COUNTRY_CODE in ('DE','AT','CH') 
-    and date_diff(DATEDIFF('day', event_date, run_at_date)) <= 29 
-    then user_id else 0 end)) as dach_active_user_count
+    and (DATEDIFF('day', event_date, current_date())) <= 29 
+    then USER_ID else '0' end)) as dach_active_user_count
     from t1
     group by 1
 )
